ba77430eacfb0022c279d5c2da7e1b3aa274b3ad
Luke Mason <lukjmason@gmail.com>
2021-05-10 16:01:33 -0600

    create function for submit button

--- src/components/NewIdeaForm/NewIdeaForm.tsx
+++ src/components/NewIdeaForm/NewIdeaForm.tsx
@@ -9,16 +9,35 @@ const NewIdeaForm = () => {
   const [description, setDescription] = useState('');
   const [title, setTitle] = useState('');
 
+  const clearInputs = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    setDescription('');
+    setTitle('');
+  }
+
   return (
     <form className='idea-form'>
       <div className='input-section'>
         <label htmlFor='title'>Title</label>
-        <input name='title' className='title' type='text' onChange={event => setTitle(event.target.value)}/>
+        <input
+          name='title'
+          className='title'
+          type='text'
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
       </div>
       <div className='input-section'>
         <label htmlFor='title'>Description</label>
-        <input name='title' className='title' type='text'onChange={event => setDescription(event.target.value)}/>
+        <input
+          name='title'
+          className='title'
+          type='text'
+          value={description}
+          onChange={event => setDescription(event.target.value)}
+        />
       </div>
+      <button className='submit-idea' onClick={clearInputs}>Submit</button>
     </form>
   )
 }
