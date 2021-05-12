5d499252ea3cb1d8153ff07ddd2105fcd97e7e48
Luke Mason <lukjmason@gmail.com>
2021-05-12 11:30:55 -0600

    Import IdeaBox component into IdeaBoxContainer component.

---
+++ src/components/IdeaBox/IdeaBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IdeaBox = () => {
+  return (
+    <p>idea</p>
+  )
+}
+
+export default IdeaBox;
--- src/components/IdeaBoxContainer/IdeaBoxContainer.tsx
+++ src/components/IdeaBoxContainer/IdeaBoxContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import IdeaBox from '../IdeaBox/IdeaBox';
 
 type Props = {
   ideas: {
