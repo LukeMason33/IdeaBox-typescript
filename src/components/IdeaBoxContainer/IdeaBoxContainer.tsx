60b534bf12a458e0a1bd80f4b2575c447b4f6cd4
Luke Mason <lukjmason@gmail.com>
2021-05-12 11:37:23 -0600

    Render IdeaBox component within IdeaBoxContainer component.

--- src/components/IdeaBoxContainer/IdeaBoxContainer.tsx
+++ src/components/IdeaBoxContainer/IdeaBoxContainer.tsx
@@ -18,10 +18,7 @@ const IdeaBoxContainer = ({ideas}: Props) => {
   const createIdeaBoxes = () =>
     ideas.map((idea:Idea) => {
       return (
-        <>
-          <p>{idea.title}</p>
-          <p>{idea.description}</p>
-        </>
+        < IdeaBox idea={idea} key={ideas.length}/>
       )
     })
 
